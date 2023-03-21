dec39020c0092408363536ec08ab753807ef687c
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-21 15:43:05 +0530

    Create CO23AD0701_5_AA1.py

---
+++ CO23AD0701_5_AA1.py
@@ -0,0 +1,9 @@
+word = 'education'
+
+print("Index of a: ", word.find('a'))
+print("Index of e: ", word.find('e'))
+print("Index of i: ", word.find('i'))
+print("Index of o: ", word.find('o'))
+print("Index of u: ", word.find('u'))
+
+print("Length of the word: ", len(word))
